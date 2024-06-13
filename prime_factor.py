83a0c805095ece9b0175d4d601b2ec8737e30e91
dayae001 <dayae001@naver.com>
2024-06-13 09:46:43 +0900

    [refactoring] 3-1

--- prime_factor.py
+++ prime_factor.py
@@ -1,8 +1,7 @@
 class PrimeFactor:
     def of(self, number) -> []:
         factors = []
-        if number == 2:
-            factors.append(number)
-        elif number == 3:
+
+        if number > 1:
             factors.append(number)
         return factors
