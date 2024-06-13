3067ca2cdb8045ac0df7db9967b8194bce6db888
dayae001 <dayae001@naver.com>
2024-06-13 09:43:58 +0900

    [refactoring] 2

--- test_prime_factor.py
+++ test_prime_factor.py
@@ -4,12 +4,13 @@ from prime_factor import PrimeFactor
 
 
 class TestPrimeFactor(TestCase):
-    def test_prime_factor_of_1(self):
-        prime_factor = PrimeFactor()
-        self.assertEqual([], prime_factor.of(1))
+    def setUp(self):
+        super().setUp()
+        self.prime_factor = PrimeFactor()
 
+    def test_prime_factor_of_1(self):
+        self.assertEqual([], self.prime_factor.of(1))
 
     def test_prime_test_of_2(self):
-        prime_Factor = PrimeFactor()
-        self.assertEqual([2], prime_Factor.of(2))
+        self.assertEqual([2], self.prime_factor.of(2))
 
