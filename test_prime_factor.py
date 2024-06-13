aee27ab0c5c837283e7e358eedd030ba6807c9dd
dayae001 <dayae001@naver.com>
2024-06-13 09:42:24 +0900

    [feature] 2

--- test_prime_factor.py
+++ test_prime_factor.py
@@ -8,3 +8,8 @@ class TestPrimeFactor(TestCase):
         prime_factor = PrimeFactor()
         self.assertEqual([], prime_factor.of(1))
 
+
+    def test_prime_test_of_2(self):
+        prime_Factor = PrimeFactor()
+        self.assertEqual([2], prime_Factor.of(2))
+
